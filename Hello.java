4f62e7f3bb851de3f9f29313e3f228c944724d60
IamPawan777 <112340386+IamPawan777@users.noreply.github.com>
2023-02-10 11:00:53 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -2,6 +2,6 @@ class Hello {
     public static void main(String[] args) {
         int no = 5;
         System.out.println("No value is: "+no);
-        System.out.println("No value is: "+no);
+        System.out.println("No value is: "+no);             //print two times
     }
 }
